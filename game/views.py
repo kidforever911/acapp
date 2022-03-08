18cd2f3110ba6792938fc5acd8b7f413231873c1
kidforever911 <pang_huadong0811@outlook.com>
2022-03-09 03:10:26 +0800

    first website

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align:center">术士之战!!!!!</h1>'
+    line2 = '<img src="https://baike.baidu.com/pic/%E6%9C%AF%E5%A3%AB%E4%B9%8B%E6%88%98/6390847/1/c2cec3fdfc039245efdd58288f94a4c27d1e25a8?fr=lemma&ct=single#aid=1&pic=c2cec3fdfc039245efdd58288f94a4c27d1e25a8">'
+    return HttpResponse(line1 + line2)

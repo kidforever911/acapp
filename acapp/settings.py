73fee8105d23464d8857b790d6e18a40b8dca1d6
kidforever911 <pang_huadong0811@outlook.com>
2022-03-18 22:40:40 +0800

    write naive login system

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kh+_mw-zst@qm859_+whv+ujum9^^2=oexp71_(d$!@2vh=gh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8.142.3.180", "game.kidforever.cn", "app713.acapp.acwing.com.cn"]
 
